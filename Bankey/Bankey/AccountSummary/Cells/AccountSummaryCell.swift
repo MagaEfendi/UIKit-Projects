a044ee1fc9fa9b7661df35f1ace9cd559bcf6b3f
Mahammad Afandiyev <maga@Mahammads-MacBook-Air.local>
2023-02-09 21:16:00 +0400

    feat: Style cell view

---
+++ Bankey/Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -0,0 +1,105 @@
+//
+//  AccountSummaryCell.swift
+//  Bankey
+//
+//  Created by Mahammad Afandiyev on 09.02.23.
+//
+
+import Foundation
+import UIKit
+
+class AccountSummaryCell : UITableViewCell {
+    let typeLabel = UILabel()
+    let underlineView = UIView()
+    let nameLabel = UILabel()
+    
+    let balanceStackView = UIStackView()
+    let balanceLabel = UILabel()
+    let balanceAmountLabel = UILabel()
+    
+    let shevronImageView = UIImageView()
+    
+    static let reuseID = "AccountSummaryCell"
+    static let rowHeight : CGFloat = 100
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AccountSummaryCell {
+    private func setup() {
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        typeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        typeLabel.adjustsFontForContentSizeCategory = true
+        typeLabel.text = "Account Type"
+        
+        underlineView.translatesAutoresizingMaskIntoConstraints = false
+        underlineView.backgroundColor = .systemTeal
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        nameLabel.adjustsFontForContentSizeCategory = true
+        nameLabel.text = "Account Name"
+        
+        balanceStackView.translatesAutoresizingMaskIntoConstraints = false
+        balanceStackView.axis = .vertical
+        balanceStackView.spacing = 0
+        
+        balanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        balanceLabel.textAlignment = .right
+        balanceLabel.text = "Some balance"
+        
+        balanceAmountLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceAmountLabel.textAlignment = .right
+        balanceAmountLabel.text = "$929,466.63"
+        
+        shevronImageView.translatesAutoresizingMaskIntoConstraints = false
+        let shevronImage = UIImage(systemName: "chevron.right")!.withTintColor(.systemTeal,renderingMode: .alwaysOriginal)
+        shevronImageView.image = shevronImage
+        
+        balanceStackView.addArrangedSubview(balanceLabel)
+        balanceStackView.addArrangedSubview(balanceAmountLabel)
+        
+        contentView.addSubview(underlineView)
+        contentView.addSubview(typeLabel)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(balanceStackView)
+        contentView.addSubview(shevronImageView)
+        
+        
+        
+    }
+    
+    private func layout() {
+        NSLayoutConstraint.activate([
+            typeLabel.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+            typeLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            
+            underlineView.topAnchor.constraint(equalToSystemSpacingBelow: typeLabel.bottomAnchor, multiplier: 1),
+            underlineView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            underlineView.heightAnchor.constraint(equalToConstant: 4),
+            underlineView.widthAnchor.constraint(equalToConstant: 60),
+            
+            nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 2),
+            nameLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            
+            balanceStackView.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 0),
+            balanceStackView.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 4),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: balanceStackView.trailingAnchor, multiplier: 4),
+            
+            shevronImageView.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: shevronImageView.trailingAnchor, multiplier: 1),
+            
+        ])
+        
+    }
+    
+}
