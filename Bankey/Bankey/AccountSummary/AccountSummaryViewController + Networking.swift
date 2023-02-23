3148cc777b58257bbcc9ada94088dc3266e33c04
Mahammad Afandiyev <maga@Mahammads-MacBook-Air.local>
2023-02-23 20:19:06 +0400

    feat: Adding refresh control

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController + Networking.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController + Networking.swift
@@ -25,6 +25,15 @@ struct Profile: Codable {
     }
 }
 
+struct Account: Codable {
+    let id: String
+    let type: AccountType
+    let name: String
+    let amount: Decimal
+    let createdDateTime: Date
+}
+
+
 extension AccountSummaryViewController {
     func fetchProfile(forUserId userId: String, completion: @escaping (Result<Profile,NetworkError>) -> Void) {
         let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
@@ -42,6 +51,30 @@ extension AccountSummaryViewController {
                 } catch {
                     completion(.failure(.decodingError))
                 }}
+        }.resume()
+    }
+}
+extension AccountSummaryViewController {
+    func fetchAccounts(forUserId userId: String, completion: @escaping (Result<[Account],NetworkError>) -> Void) {
+        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)/accounts")!
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    completion(.failure(.serverError))
+                    return
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    
+                    let accounts = try decoder.decode([Account].self, from: data)
+                    completion(.success(accounts))
+                } catch {
+                    completion(.failure(.decodingError))
+                }
+            }
         }.resume()
     }
 }
--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -9,12 +9,12 @@ class AccountSummaryViewController: UIViewController {
     
     //View Models
     var headerViewModel = AccountSummaryHeaderView.ViewModel(welcomeMessage: "Welcome", name: "", date: Date())
-    
     var accountsCellViewModels : [AccountSummaryCell.ViewModel] = []
     
+    //Components
     var tableView = UITableView()
     let headerView = AccountSummaryHeaderView(frame: .zero)
-    
+    let refreshControl = UIRefreshControl()
    
     
     lazy var logoutBarButtonItem: UIBarButtonItem = {
@@ -39,8 +39,8 @@ extension AccountSummaryViewController {
         setupTableView()
         setupTableHeaderView()
         setupNavigationBar()
-        fetchDataAndLoadViews()
-
+        setupRefreshControl()
+        fetchData()
     }
     
     private func setupTableView() {
@@ -62,6 +62,7 @@ extension AccountSummaryViewController {
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
     private func setupTableHeaderView() {
         
         var size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
@@ -70,8 +71,15 @@ extension AccountSummaryViewController {
         
         tableView.tableHeaderView = headerView
     }
+    
     func setupNavigationBar() {
         navigationItem.rightBarButtonItem = logoutBarButtonItem
+    }
+    
+    private func setupRefreshControl() {
+        refreshControl.tintColor = appColor
+        refreshControl.addTarget(self, action: #selector(refreshContent), for: .valueChanged)
+        tableView.refreshControl = refreshControl
     }
 }
 
@@ -98,14 +106,9 @@ extension AccountSummaryViewController: UITableViewDelegate {
     }
 }
 
-extension AccountSummaryViewController {
-    @objc func logoutTapped(sender: UIButton) {
-        NotificationCenter.default.post(name: .logout, object: nil)
-    }
-}
 // MARK: - Networking
 extension AccountSummaryViewController {
-    private func fetchDataAndLoadViews() {
+    private func fetchData() {
         let group = DispatchGroup()
         
         group.enter()
@@ -132,6 +135,7 @@ extension AccountSummaryViewController {
         }
         group.notify(queue: .main){
             self.tableView.reloadData()
+            self.tableView.refreshControl?.endRefreshing()
         }
     }
     
@@ -148,35 +152,16 @@ extension AccountSummaryViewController {
         }
     }
 }
-struct Account: Codable {
-    let id: String
-    let type: AccountType
-    let name: String
-    let amount: Decimal
-    let createdDateTime: Date
-}
 
+
+
+// MARK: - Actions
 extension AccountSummaryViewController {
-    func fetchAccounts(forUserId userId: String, completion: @escaping (Result<[Account],NetworkError>) -> Void) {
-        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)/accounts")!
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                guard let data = data, error == nil else {
-                    completion(.failure(.serverError))
-                    return
-                }
-                
-                do {
-                    let decoder = JSONDecoder()
-                    decoder.dateDecodingStrategy = .iso8601
-                    
-                    let accounts = try decoder.decode([Account].self, from: data)
-                    completion(.success(accounts))
-                } catch {
-                    completion(.failure(.decodingError))
-                }
-            }
-        }.resume()
+    @objc func logoutTapped(sender: UIButton) {
+        NotificationCenter.default.post(name: .logout, object: nil)
+    }
+    
+    @objc func refreshContent() {
+        fetchData()
     }
 }
